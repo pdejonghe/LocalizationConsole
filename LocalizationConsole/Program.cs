c5416382cf8468e41221df28df4c22e9b7075f7e
pdejonghe <dejonghe.peter@gmail.com>
2018-12-18 14:33:02 +0100

    Initial commit

---
+++ LocalizationConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LocalizationConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
