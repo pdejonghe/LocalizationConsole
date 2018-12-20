be554a3902b800a9f6feecd14e6863c6c215604b
pdejonghe <dejonghe.peter@gmail.com>
2018-12-20 18:16:29 +0100

    Renames to make code more readable

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,17 +12,17 @@ namespace Models
         public override LocalizedProduct GetLocalizedEntity(string cultureCode)
         {
             var localizedProduct = Activator.CreateInstance<LocalizedProduct>();
-            var localizableProduct = LocalizablePropertySets.FirstOrDefault(p => p.CultureCode == cultureCode)
+            var localizedProductPropertySet = LocalizablePropertySets.FirstOrDefault(p => p.CultureCode == cultureCode)
                                         ?? LocalizablePropertySets.FirstOrDefault(p => p.CultureCode == "en-US")
                                         ?? LocalizablePropertySets.FirstOrDefault();
 
             localizedProduct.Id = this.Id;
             localizedProduct.Price = this.Price;
 
-            if (null != localizableProduct)
+            if (null != localizedProductPropertySet)
             {
-                localizedProduct.Name = localizableProduct.Name;
-                localizedProduct.Description = localizableProduct.Description;
+                localizedProduct.Name = localizedProductPropertySet.Name;
+                localizedProduct.Description = localizedProductPropertySet.Description;
             }
 
             return localizedProduct;
